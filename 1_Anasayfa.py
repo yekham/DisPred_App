de033f6f16dd6b435c1456992499afc9a97c5a47
Yekta Hamit Pektaş <yektahamit@gmail.com>
2024-01-06 23:27:36 +0300

    Update 1_Anasayfa.py

--- 1_Anasayfa.py
+++ 1_Anasayfa.py
@@ -7,3 +7,6 @@ st.set_page_config(
 
 st.title("DİSPRED")
 st.write("DİSPRED uygulamasına hoşgeldiniz.")
+
+intro_markdown = read_markdown_file("about.md")
+st.markdown(intro_markdown, unsafe_allow_html=True)
