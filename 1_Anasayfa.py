edb97dfe8836522560f9b65145bb61e73b6e2c8c
Yekta Hamit Pektaş <yektahamit@gmail.com>
2024-01-06 23:31:43 +0300

    Update 1_Anasayfa.py

--- 1_Anasayfa.py
+++ 1_Anasayfa.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from pathlib import Path
 
 st.set_page_config(
     page_title="DisPred",
@@ -7,6 +8,8 @@ st.set_page_config(
 
 st.title("DİSPRED")
 st.write("DİSPRED uygulamasına hoşgeldiniz.")
+def read_markdown_file(markdown_file):
+    return Path(markdown_file).read_text()
 
 intro_markdown = read_markdown_file("about.md")
 st.markdown(intro_markdown, unsafe_allow_html=True)
