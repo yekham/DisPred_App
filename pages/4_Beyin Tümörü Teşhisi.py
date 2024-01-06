95f1e3e44d2b1df9ce33795a52f13803f03ed356
Yekta Hamit Pektaş <yektahamit@gmail.com>
2024-01-06 22:50:48 +0300

    Update 4_Beyin Tümörü Teşhisi.py

--- pages/4_Beyin Tümörü Teşhisi.py
+++ pages/4_Beyin Tümörü Teşhisi.py
@@ -53,7 +53,7 @@ if uploaded_file is not None:
     predicted_class = predict_tumor(img_array)
 
     # Sonucu göster
-    st.image(image, height=300,use_column_width=True)
+    st.image(image, use_column_width=True)
     if predicted_class == 'no_tumor':
         st.write("Tahmin edilen sınıf: no_tumor")
     elif predicted_class == 'meningioma_tumor':
