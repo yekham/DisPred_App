5df7953f1133c4c62e5cd761b059e7288b59942b
Yekta Hamit Pektaş <yektahamit@gmail.com>
2024-01-07 22:15:39 +0300

    Update 1_Anasayfa.py

--- 1_Anasayfa.py
+++ 1_Anasayfa.py
@@ -14,7 +14,7 @@ hide_streamlit_style = """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 st.markdown("<h1 style='text-align: center; color: red;'>DisPred</h1>", unsafe_allow_html=True)
-st.image(Image.open("dispred.jpg"))
+st.image(Image.open("dispred.jpg"),use_column_width=True)
 st.write(
     "Merhaba! Yapay Zeka ile Hastalık Tahmini Uygulamamıza hoş geldiniz. Bu uygulama, belirli semptomlara dayanarak "
     "bir hastalığın olası olup olmadığını tahmin etmeye yardımcı olur. Hemen başlamak için sol taraftaki "
