6d2d2529dc8aadd421f3a2d07bdd34ad0de50bbd
Yekta Hamit Pektaş <yektahamit@gmail.com>
2024-01-07 22:02:59 +0300

    Update 1_Anasayfa.py

--- 1_Anasayfa.py
+++ 1_Anasayfa.py
@@ -12,4 +12,4 @@ hide_streamlit_style = """
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-
+st.markdown("<h1 style='text-align: center; color: red;'>DisPred</h1>", unsafe_allow_html=True)
