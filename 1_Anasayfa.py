2c45d3ae4293cac5172f506a4f665bc9b7cf67bf
Yekta Hamit Pektaş <yektahamit@gmail.com>
2024-01-07 22:11:43 +0300

    Update 1_Anasayfa.py

--- 1_Anasayfa.py
+++ 1_Anasayfa.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 
 st.set_page_config(
     page_title="DisPred",
@@ -13,3 +14,5 @@ hide_streamlit_style = """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 st.markdown("<h1 style='text-align: center; color: red;'>DisPred</h1>", unsafe_allow_html=True)
+st.image(Image.open("dispred.jpg"))
+st.write("DisPred uygulamasına Hosgeldiniz!")
