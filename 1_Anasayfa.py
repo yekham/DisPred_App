e511c9f8a18d729f41f19410f59ab0cff860a987
Yekta Hamit Pektaş <yektahamit@gmail.com>
2024-01-07 22:13:35 +0300

    Update 1_Anasayfa.py

--- 1_Anasayfa.py
+++ 1_Anasayfa.py
@@ -15,4 +15,8 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 st.markdown("<h1 style='text-align: center; color: red;'>DisPred</h1>", unsafe_allow_html=True)
 st.image(Image.open("dispred.jpg"))
-st.write("DisPred uygulamasına Hosgeldiniz!")
+st.write(
+    "Merhaba! Yapay Zeka ile Hastalık Tahmini Uygulamamıza hoş geldiniz. Bu uygulama, belirli semptomlara dayanarak "
+    "bir hastalığın olası olup olmadığını tahmin etmeye yardımcı olur. Hemen başlamak için sol taraftaki "
+    "paneli kullanabilirsiniz."
+)
