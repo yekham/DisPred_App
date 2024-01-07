73f9471c4ad217464c4868541df2fe16b306676a
Yekta Hamit Pektaş <yektahamit@gmail.com>
2024-01-07 20:49:23 +0300

    Update 1_Anasayfa.py

--- 1_Anasayfa.py
+++ 1_Anasayfa.py
@@ -1,9 +1,6 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="DisPred",
-    page_icon="🩺",
-)
+
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
@@ -11,3 +8,31 @@ hide_streamlit_style = """
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
+def set_page_title(title):
+    st.sidebar.markdown(unsafe_allow_html=True, body=f"""
+        <iframe height=0 srcdoc="<script>
+            const title = window.parent.document.querySelector('title') \
+
+            const oldObserver = window.parent.titleObserver
+            if (oldObserver) {{
+                oldObserver.disconnect()
+            }} \
+
+            const newObserver = new MutationObserver(function(mutations) {{
+                const target = mutations[0].target
+                if (target.text !== '{title}') {{
+                    target.text = '{title}'
+                }}
+            }}) \
+
+            newObserver.observe(title, {{ childList: true }})
+            window.parent.titleObserver = newObserver \
+
+            title.text = '{title}'
+        </script>" />
+    """)
+
+
+set_page_title("🩺DisPred")
